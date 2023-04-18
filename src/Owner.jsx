c09c2dabc5196b39c6aa8778e69d894640cae4e9
Haluethaichanok-J <haluethaichanok@gmail.com>
2023-04-18 10:34:25 +0700

    feat:Owner

---
+++ src/Owner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Layout from "./Layout";
+const Owner = () =>{
+
+    return(
+        <Layout>
+            <h1>Praew - Group E - 10</h1>
+            <div className="img">
+            <img width = "300px" src='https://static.thairath.co.th/media/dFQROr7oWzulq5Fa5nRRVgnzYSSwUoPM7rigVHaj4QhdURLfyt90hBPNzf89n8vZ5bp.webp'/>
+            </div>
+            <div>
+            <h5>Short Biography</h5>
+<p>Two distinct gene loci, the agouti gene locus (two alleles) and the tabby locus (three alleles), and one modifier, spotted (two alleles), cause the four basic tabby patterns. The fifth pattern is emergent, being expressed by female cats with one black and one orange gene on each of their two X chromosomes, and is explained by Barr bodies and the genetics of sex-linked inheritance.</p>
+            </div>
+            
+
+        </Layout>
+    )
+}
+
+
+export default Owner
