4d9a86b740bd86ca90e440defbb7a5c6f8fd31c9
Haluethaichanok-J <haluethaichanok@gmail.com>
2023-04-18 16:42:01 +0700

    test 2 feature

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react"
 import Layout from "./Layout"
 
-const mockEmployees = [
+const employees = [
   {
     id: 0,
     name: "mock",
@@ -19,55 +19,67 @@ const mockEmployees = [
     name: "employee 2",
     lastname: "lord",
     position: "Designer"
-  },
+  }
 ]
 
+
+
 const Home = () => {
-const [employees,setEmployees] = useState('')
+// const [employees,setEmployees] = useState('')
 const [sector,setSector] =useState(false)
 const [showUser,setShowUser] = useState(false)
 const [showAdmin,setShowAdmin] = useState(false)
 
-const [a,setA] =useState('');
-// useEffect(()=>{
-//   var value;
-// if(sector === 'true' && value=== 'user'){
-// setA('user')
-// }
-// else if(sector === 'true' && value=== 'admin'){
-//  setA('admin')
-// }
-// else{
-// setA('')
-// }
-// },[sector])
-// useEffect(()=>
-// {
-//   if(showUser === true){
+
+
+
+const showEmployee = (item) => {
+  return(
+    <tr>
+
+      <td>{item.name}</td>
+      <td>{item.lastname}</td>
+      <td>{item.position}</td>
+    </tr>
+  )
+}
+employees.map(showEmployee)
+
+// const [a,setA] =useState('');
+// useEffect(()=>{if(showUser === true){
 //   setA('user')
-//   }
-//   if(showAdmin === true){
-//   setA('Admin')
 //   }
 //   else{
 //     setA('')
-//   }
-//   },[showUser, showAdmin])
-useEffect(()=>{if(showUser === true){
-  setA('user')
+//   } 
+// },[showUser])
+//   useEffect(()=>{if(showAdmin === true){
+//       setA('Admin')
+//       }
+//       else{
+//         setA('')
+//       }
+//     },[ showAdmin])
+
+function MyFunc(){
+  var tb = document.getElementById('table');
+  if(tb.style.display === 'none'){
+    tb.style.display = 'inline'
   }
   else{
-    setA('')
-  } 
-},[showUser])
-  useEffect(()=>{if(showAdmin === true){
-      setA('Admin')
-      }
-      else{
-        setA('')
-      }
-    },[ showAdmin])
+    tb.style.display = 'none'
+  }
+}
 
+function MyFunc2(){
+  var tb = document.getElementById('table2');
+  if(tb.style.display === 'none'){
+    tb.style.display = 'inline'
+  }
+  else{
+    tb.style.display = 'none'
+  }
+}
   return (
     <>
     <Layout/>
@@ -76,10 +88,47 @@ useEffect(()=>{if(showUser === true){
       <h1>React - Assessment</h1>
     </div>
 <div>
-<button onClick={()=>setShowUser(true)}>User Home Sector</button>
-<button onClick={()=>setShowAdmin(true)}>Admin Home Sector</button>
+{/* <button onClick={()=>setShowUser(true)}>User Home Sector</button>
+<button onClick={()=>setShowAdmin(true)}>Admin Home Sector</button> */}
+
+<button onClick={MyFunc} >User Home Sector</button>
+<button onClick={MyFunc2} >Admin Home Sector</button>
 </div>
-{a}
+{/* {a} */}
+<div id="table" style={{display: 'none'}}>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Last Name</th>
+        <th>Position</th>
+      </tr>
+    </thead>
+    <tbody>
+      {employees.map(showEmployee)}
+    </tbody>
+  </table>
+</div>
+<div id="table2" style={{display: 'none'}}>
+<h3>Create User Here</h3>
+<input type="text" placeholder="Name"/>
+<input type="text" placeholder="Last Name"/>
+<input type="text"placeholder="Position"/>
+<button>Save</button>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Last Name</th>
+        <th>Position</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {employees.map(showEmployee)}
+    </tbody>
+  </table>
+</div>
 </>
 
    
