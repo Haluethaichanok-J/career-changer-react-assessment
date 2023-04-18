adb7009edd86ccd0ec563eafbbb5fee847e59894
Haluethaichanok-J <haluethaichanok@gmail.com>
2023-04-18 14:30:46 +0700

    test feature

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import Layout from "./Layout"
 
 const mockEmployees = [
@@ -23,8 +23,51 @@ const mockEmployees = [
 ]
 
 const Home = () => {
-const [employees,setEmployees] = useState()
-const [sector,setSector] =useState()
+const [employees,setEmployees] = useState('')
+const [sector,setSector] =useState(false)
+const [showUser,setShowUser] = useState(false)
+const [showAdmin,setShowAdmin] = useState(false)
+
+const [a,setA] =useState('');
+// useEffect(()=>{
+//   var value;
+// if(sector === 'true' && value=== 'user'){
+// setA('user')
+// }
+// else if(sector === 'true' && value=== 'admin'){
+//  setA('admin')
+// }
+// else{
+// setA('')
+// }
+// },[sector])
+// useEffect(()=>
+// {
+//   if(showUser === true){
+//   setA('user')
+//   }
+//   if(showAdmin === true){
+//   setA('Admin')
+//   }
+//   else{
+//     setA('')
+//   }
+//   },[showUser, showAdmin])
+useEffect(()=>{if(showUser === true){
+  setA('user')
+  }
+  else{
+    setA('')
+  } 
+},[showUser])
+  useEffect(()=>{if(showAdmin === true){
+      setA('Admin')
+      }
+      else{
+        setA('')
+      }
+    },[ showAdmin])
+
   return (
     <>
     <Layout/>
@@ -33,9 +76,10 @@ const [sector,setSector] =useState()
       <h1>React - Assessment</h1>
     </div>
 <div>
-<button>User Home </button>
-<button>Admin Home Sector</button>
+<button onClick={()=>setShowUser(true)}>User Home Sector</button>
+<button onClick={()=>setShowAdmin(true)}>Admin Home Sector</button>
 </div>
+{a}
 </>
 
    
