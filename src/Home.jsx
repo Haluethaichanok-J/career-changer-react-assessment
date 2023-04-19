1e21a5c3aad44f002be6d0476bcabcc2fd5c868a
Haluethaichanok-J <haluethaichanok@gmail.com>
2023-04-19 09:33:07 +0700

    feat : Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,8 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useState, Component} from "react"
 import Layout from "./Layout"
+import { v4 as uuidv4 } from 'uuid';
 
-const employees = [
-  {
-    id: 0,
-    name: "mock",
-    lastname: 'mocklastname',
-    position: "Manager"
-  },
-  {
-    id: 1,
-    name: "employee 1",
-    lastname: "em",
-    position: "Engineer"
-  },
-  {
-    id: 2,
-    name: "employee 2",
-    lastname: "lord",
-    position: "Designer"
-  }
-]
+
 
 
 
@@ -29,22 +11,56 @@ const Home = () => {
 const [sector,setSector] =useState(false)
 const [showUser,setShowUser] = useState(false)
 const [showAdmin,setShowAdmin] = useState(false)
+const [employees,setEmployees] =useState([])
+// console.log(list)
 
+const [item,setItem] =useState([employees])
+console.log(item)
 
-
-
-const showEmployee = (item) => {
+const showEmployee = (item,i) => {
+// const newItem = [item];
+  console.log(item);
+  // console.log(newItem)
   return(
     <tr>
 
       <td>{item.name}</td>
       <td>{item.lastname}</td>
       <td>{item.position}</td>
+  
+    <td><button onClick={()=>deleteItem(item.id)}>Delete</button></td>
     </tr>
   )
 }
 employees.map(showEmployee)
 
+
+function deleteItem(id){
+ 
+//   const newItem = item.filter(t =>t.id !== id)
+//   console.log(newItem)
+//   setItem(newItem)
+// setEmployees(newItem)
+setEmployees(item.filter(it => it.id !== id))
+}
+
+
+
+const showEmployee2 = (item) => {
+  return(
+    <tr>
+      <td>{item.name}</td>
+      <td>{item.lastname}</td>
+      <td>{item.position}</td>
+     
+    </tr>
+  )
+}
+employees.map(showEmployee2)
+
+
+// let arr;
+// employees.map(arr);
 // const [a,setA] =useState('');
 // useEffect(()=>{if(showUser === true){
 //   setA('user')
@@ -60,15 +76,46 @@ employees.map(showEmployee)
 //         setA('')
 //       }
 //     },[ showAdmin])
+const [name,setName] = useState('');
+const [lastname, setLastName] =useState ('')
+const [position, setPosition] = useState ('')
+
+const inputName = (event) =>{
+setName(event.target.value)
+}
+const inputLastName = (event) =>{
+setLastName(event.target.value)
+}
+const inputPosition = (event) =>{
+setPosition(event.target.value)
+}
+const saveItem = (event) =>{
+  event.preventDefault()
+  const inputData = {
+    id: uuidv4(),
+    name:name,
+    lastname:lastname,
+    position:position
+  }
+  employees.push({...inputData})
+  console.log(employees);
+//console.log(inputData);
+setName('')
+setLastName('')
+setPosition('')
+}
 
 function MyFunc(){
   var tb = document.getElementById('table');
+ 
   if(tb.style.display === 'none'){
     tb.style.display = 'inline'
+  
   }
   else{
     tb.style.display = 'none'
   }
+
 }
 
 function MyFunc2(){
@@ -105,16 +152,17 @@ function MyFunc2(){
       </tr>
     </thead>
     <tbody>
-      {employees.map(showEmployee)}
+      {employees.map(showEmployee2)}
+      
     </tbody>
   </table>
 </div>
 <div id="table2" style={{display: 'none'}}>
 <h3>Create User Here</h3>
-<input type="text" placeholder="Name"/>
-<input type="text" placeholder="Last Name"/>
-<input type="text"placeholder="Position"/>
-<button>Save</button>
+<input type="text" placeholder="Name" onChange={inputName} value={name}/>
+<input type="text" placeholder="Last Name" onChange={inputLastName} value={lastname}/>
+<input type="text" placeholder="Position" onChange={inputPosition} value={position}/>
+<button onClick={saveItem}>Save</button>
   <table>
     <thead>
       <tr>
@@ -126,6 +174,8 @@ function MyFunc2(){
     </thead>
     <tbody>
       {employees.map(showEmployee)}
+
+
     </tbody>
   </table>
 </div>
